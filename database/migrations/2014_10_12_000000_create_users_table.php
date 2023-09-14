dd89e015cb1769e3360e2173eb2b011728d3a932
MarioSantoro <mariosantoro124@gmail.com>
2023-09-14 16:11:08 +0200

    change migration of user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,14 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('vat_number', 11)->unique();
+            $table->string('address')->unique();
+            $table->time('closer_time');
+            $table->time('open_time');
+            $table->string('free_delivery_from')->nullable();
+            $table->text('logo');
+            $table->text('image');
+            $table->string('phone')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
