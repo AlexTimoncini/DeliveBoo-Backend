b165a36b0baad5d012c8a45431b379251142f2bb
AlexTimoncini <alextimonci@gmail.com>
2023-09-14 16:37:55 +0200

    seeded types

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $dataPath = base_path('database/data');
+        $json = File::get($dataPath.'/type_list.json');
+        $data = json_decode($json, true);
+        foreach($data['types'] as $type){
+            $newType = new Type();
+            $newType->name = $type['name'];
+            $newType->logo = $type['logo'];
+            $newType->save();
+        }
+
     }
 }
