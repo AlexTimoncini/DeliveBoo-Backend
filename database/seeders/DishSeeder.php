ce3a279fcde8edd99342e7a2f3b81ea2b69f7e6e
EmanuelaPau <uapame@gmail.com>
2023-09-14 16:56:56 +0200

    Add Database seeder

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Dish;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class DishSeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class DishSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $dataPath = base_path('database/data');
+        $json = File::get($dataPath . '/dish_list.json');
+        $data = json_decode($json, true);
+
+        foreach ($data['dishes'] as $dish) {
+            $newDish = new Dish;
+            $newDish->name = $dish['name'];
+            $newDish->description = $dish['description'];
+            $newDish->price = $dish['price'];
+            $newDish->photo = $dish['photo'];
+            $newDish->available = $dish['availability'];
+            $newDish->visible = $dish['visible'];
+            $newDish->save();
+        }
         //
     }
 }
+;
