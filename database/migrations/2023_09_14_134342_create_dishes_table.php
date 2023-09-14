927877588e020f774adff8e04dbdb77546325fae
EmanuelaPau <uapame@gmail.com>
2023-09-14 15:57:36 +0200

    Add Dish migration

--- database/migrations/2023_09_14_134342_create_dishes_table.php
+++ database/migrations/2023_09_14_134342_create_dishes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('dishes', function (Blueprint $table) {
             $table->id();
+            $table->text('name');
+            $table->text('description')->nullable();
+            $table->float('price', 5, 2);
+            $table->text('photo');
+            $table->boolean('available');
+            $table->boolean('visible');
             $table->timestamps();
         });
     }
