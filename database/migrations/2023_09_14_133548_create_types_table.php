b0f3db879d1488847bbc34fde5cdc4446b725319
AlexTimoncini <alextimonci@gmail.com>
2023-09-14 15:42:56 +0200

    fixed types migration

--- database/migrations/2023_09_14_133548_create_types_table.php
+++ database/migrations/2023_09_14_133548_create_types_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->text('logo')->unique();
+            $table->string('name');
+            $table->text('logo');
             $table->timestamps();
         });
     }
