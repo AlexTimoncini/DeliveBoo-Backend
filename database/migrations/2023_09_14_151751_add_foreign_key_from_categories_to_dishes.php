320e9917dade557fed0ae32abf53384451b489b8
MarioSantoro <mariosantoro124@gmail.com>
2023-09-14 17:43:51 +0200

    created a relationship from categories table to dishes table

---
+++ database/migrations/2023_09_14_151751_add_foreign_key_from_categories_to_dishes.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->after('id');
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('dishes', function (Blueprint $table) {
+            $table->dropForeign('dishes_category_id_foreign');
+            $table->dropColumn('category_id');
+        });
+    }
+};
--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Dish;
-use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
@@ -17,20 +17,24 @@ class DishSeeder extends Seeder
     {
 
         $dataPath = base_path('database/data');
-        $json = File::get($dataPath . '/dish_list.json');
+        $json = File::get($dataPath . '/restaurant_list.json');
         $data = json_decode($json, true);
 
-        foreach ($data['dishes'] as $dish) {
-            $newDish = new Dish;
-            $newDish->name = $dish['name'];
-            $newDish->description = $dish['description'];
-            $newDish->price = $dish['price'];
-            $newDish->photo = $dish['photo'];
-            $newDish->available = $dish['availability'];
-            $newDish->visible = $dish['visible'];
-            $newDish->save();
+
+
+        foreach ($data['restaurants'] as $restaurant) {
+            foreach ($restaurant['dish_list'] as $dish) {
+                $newDish = new Dish;
+                $newDish->name = $dish['name'];
+                $newDish->category_id = Category::where('name', '=', $dish['category'])->get()->pluck('id')[0];
+                $newDish->description = $dish['description'];
+                $newDish->price = $dish['price'];
+                $newDish->photo = $dish['photo'];
+                $newDish->available = $dish['availability'];
+                $newDish->visible = 1;
+                $newDish->save();
+            }
         }
         //
     }
-}
-;
+};
