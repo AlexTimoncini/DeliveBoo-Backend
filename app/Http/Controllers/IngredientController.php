01b14b096dea08edf88157ca1a434d6c5058d02e
AlexTimoncini <alextimonci@gmail.com>
2023-09-14 15:46:31 +0200

    added every resource for deliveboo backoffice

---
+++ app/Http/Controllers/IngredientController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ingredient;
+use Illuminate\Http\Request;
+
+class IngredientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ingredient $ingredient)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ingredient $ingredient)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ingredient $ingredient)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ingredient $ingredient)
+    {
+        //
+    }
+}
