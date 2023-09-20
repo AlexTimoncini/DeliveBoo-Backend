c65e20a6b47241a28e1535ea5af4b104e6b62f98
MarioSantoro <mariosantoro124@gmail.com>
2023-09-20 12:30:10 +0200

    added method for clear exteption  unauthenticated

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,25 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($exception instanceof AuthenticationException) {
+            // Restituisci una risposta vuota senza dati o messaggi
+            return response()->noContent();
+        }
+    }
+    protected function prepareResponse($request, Throwable $e)
+    {
+        return parent::prepareResponse($request, $e);
+    }
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
