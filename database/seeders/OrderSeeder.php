2b6f40f0f17b7acfe01d20a08ce57928e72e5556
AlexTimoncini <alextimonci@gmail.com>
2023-09-22 10:48:43 +0200

    fixed order seeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -3,17 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\Order;
+use App\Models\Dish;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
+use Faker\Generator as Faker;
 
 class OrderSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         $dataPath = base_path('database/data');
         $json = File::get($dataPath . '/restaurant_list.json');
@@ -24,13 +26,18 @@ class OrderSeeder extends Seeder
                 $newOrder = new Order();
                 $newOrder->user_id = $restaurant_id;
                 $newOrder->customer_address = $order['customer_address'];
-                $newOrder->first_name = 'Paolo';
-                $newOrder->last_name = 'Conti';
-                $newOrder->interior = 'ciao';
-                $newOrder->doorbell = 'gino';
-                $newOrder->phone = '0000000000';
-                $newOrder->successful = 1;
-                $newOrder->total_price = 100.00;
+                $newOrder->first_name = $faker->firstName();
+                $newOrder->last_name = $faker->lastName();
+                $newOrder->interior = $faker->buildingNumber();
+                $newOrder->doorbell = $newOrder->first_name . ' ' . $newOrder->last_name;
+                $newOrder->phone = $faker->phoneNumber();
+                $newOrder->successful = $faker->boolean();
+                $totalPrice = 0;
+                foreach ($order['dish_list'] as $dish ) {
+                    $dishPrice = Dish::where('name', $dish)->get()->pluck('price')[0];
+                    $totalPrice += $dishPrice;
+                }
+                $newOrder->total_price = $totalPrice;
                 $newOrder->save();
             }
         }
