d8cbc9a5b59620eeeb6274a7c9ad43a3db5dc663
AlexTimoncini <alextimonci@gmail.com>
2023-09-22 12:09:04 +0200

    create api route for ranking restaurants

--- routes/api.php
+++ routes/api.php
@@ -26,19 +26,23 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::get('/types', [TypeController::class, 'index']);
 Route::get('/categories', [CategoryController::class, 'index']);
+
 Route::get('/restaurants/bestSeller', [RestaurantController::class, 'bestRestaurants']);
 Route::get('/restaurants/newInTown', [RestaurantController::class, 'newInTown']);
-Route::get('/dishes/{id}', [DishController::class, 'show']);
-Route::post('/store', [DishController::class, 'store']);
-Route::put('/update/{dish}', [DishController::class, 'update']);
-Route::delete('/delete/{dish}', [DishController::class, 'delete']);
+Route::get('/restaurants/{id}/ranking', [RestaurantController::class, 'ranking']);
+Route::get('/restaurants/logo/{id}', [ImagesController::class, 'show']);
 Route::get('/restaurants/{id}', [RestaurantController::class, 'show']);
 Route::put('/update/restaurant/{restaurant}', [RestaurantController::class, 'update']);
 Route::get('/restaurants/{id}/dishes', [RestaurantController::class, 'indexDishes']);
 Route::get('/restaurants/search/advance/{query}', [RestaurantController::class, 'advancedSearch']);
-Route::get('/orders/{id}', [OrderController::class, 'show']);
-Route::get('/dish/{id}', [ImagesController::class, 'showDishes']);
-Route::get('/restaurants/logo/{id}', [ImagesController::class, 'show']);
-Route::post('/upload/File/{id}', [DishController::class, 'upload']);
 Route::post('/upload/restaurants/logo/File/{id}', [RestaurantController::class, 'uploadLogo']);
 Route::post('/upload/restaurants/image/File/{id}', [RestaurantController::class, 'uploadImage']);
+
+Route::post('/store', [DishController::class, 'store']);
+Route::put('/update/{dish}', [DishController::class, 'update']);
+Route::delete('/delete/{dish}', [DishController::class, 'delete']);
+Route::get('/orders/{id}', [OrderController::class, 'show']);
+Route::post('/upload/File/{id}', [DishController::class, 'upload']);
+Route::get('/dishes/{id}', [DishController::class, 'show']);
+
+Route::get('/dish/{id}', [ImagesController::class, 'showDishes']);
