942376f83af4c5dea295fc4457b09416165f5e75
MarioSantoro <mariosantoro124@gmail.com>
2023-09-29 15:58:29 +0200

    change style of email resetLink

--- resources/views/vendor/mail/html/panel.blade.php
+++ resources/views/vendor/mail/html/panel.blade.php
@@ -1,14 +1,13 @@
-<table class="panel" width="100%" cellpadding="0" cellspacing="0" role="presentation">
-<tr>
-<td class="panel-content">
-<table width="100%" cellpadding="0" cellspacing="0" role="presentation">
-<tr>
-<td class="panel-item">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
-</td>
-</tr>
+<table width="100%" cellpadding="0" cellspacing="0" style="background-color: #01975c !important">
+    <tr>
+        <td class="panel-content">
+            <table width="100%" cellpadding="0" cellspacing="0" style="background-color: #01975c">
+                <tr>
+                    <td class="panel-item">
+                        {{ Illuminate\Mail\Markdown::parse($slot) }}
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
 </table>
-</td>
-</tr>
-</table>
-
